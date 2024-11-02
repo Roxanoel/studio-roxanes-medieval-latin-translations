6f5deb6c617cea96246de9a569fb1d2149e3b6bd
Roxane Noël <roxane.noel92@gmail.com>
2024-11-02 12:50:38 -0700

    added studio host to config

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'qzf815li',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'roxaneslatintranslations',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
